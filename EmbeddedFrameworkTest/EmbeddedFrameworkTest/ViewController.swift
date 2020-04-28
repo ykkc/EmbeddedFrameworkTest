2a90641ea81826d32dcf6ae2556fb85b9f38a47d
ykkc <ylane.admirable@gmail.com>
2020-04-28 12:54:49 +0900

    :sparkles: init project

---
+++ EmbeddedFrameworkTest/EmbeddedFrameworkTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EmbeddedFrameworkTest
+//
+//  Created by ykkc on 2020/04/28.
+//  Copyright © 2020 ykkc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
